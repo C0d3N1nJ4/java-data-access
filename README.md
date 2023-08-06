65309e824d34bc0bba98befd2611d555a7d3f3dc
naiomi <Naiomi.Naidoo@icloud.com>
2023-08-06 11:35:06 +0200

    Included resources required for the database

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# java-data-access
+# java-data-access with JDBC
+
+Setting up the database
+1. Run the postgresql docker container under resources
+2. Create the database with psql.sh under resources
+
+## Steps
+1. Create Vehicle class
+2. Create VehicleDao interface
+3. Configuration
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,51 @@
+package src.main.java;
+
+public class Vehicle {
+
+    private String VehicleNo;
+
+    private String color;
+
+    private int wheel;
+
+    private int seat;
+
+    public Vehicle(String vehicleNo, String color, int wheel, int seat) {
+        VehicleNo = vehicleNo;
+        this.color = color;
+        this.wheel = wheel;
+        this.seat = seat;
+    }
+
+    public String getVehicleNo() {
+        return VehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        VehicleNo = vehicleNo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWheel() {
+        return wheel;
+    }
+
+    public void setWheel(int wheel) {
+        this.wheel = wheel;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+}
